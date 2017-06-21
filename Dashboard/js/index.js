379e370e161b3c8658d597b5b2056f97ba5347ff
SuWenYu <suwenyu@SuWenYuh.local>
2017-06-21 14:32:33 +0800

    add monitor and admin

--- Dashboard/js/index.js
+++ Dashboard/js/index.js
@@ -14,30 +14,67 @@ var deleteSensor = function(selctor) {
 
 }
 
-
-
-
-
-
-$('document').ready(function() {
-
-    $.getJSON( "../sensor_data.json", function( data ) {
-        console.log( data );
-    });
-
-
-
-    $('.sensor-close').on('click', function(event) {
-    	var _this = $(this);
+function close(){
+    $('.sensor-close').click(function() {
+        var _this = $(this);
         var conf = {
             modal: '#indexModal',
             title: '移除感測器',
             content: '確認移除感測器嗎？',
             callback_target: '.btn-danger',
             callback:function(){
-            	$(_this).parents('.col').remove();
+                $(_this).parents('.col').remove();
             }
         };
         addModalContent(conf);
-    })
+    });
+}
+
+
+
+$('document').ready(function() {
+
+    $.getJSON( "https://suwenyu.github.io/test_sensor/sensor_data.json", function( data ) {
+        $.each( data, function( key, val ) {
+            console.log(key, val);
+
+            $('#sensor_data').append(
+                '<div class="col col-12 col-sm-6 col-md-4">'+
+                    '<div class="sensor-item">'+
+                        '<div class=row>'+
+                            '<div class="col-10">'+
+                                '<div class="sensor-mark">'+
+                                    'Sensor ' + val['sensor_num']+
+                                '</div>'+
+                            '</div>'+
+                            '<div class="col-2">'+
+                                '<div class="sensor-close F-right">'+
+                                    '<i class="fa fa-times"></i>'+
+                                '</div>'+
+                            '</div>'+
+                        '</div>'+
+                    '<hr class="sensor-hr">'+
+                        '<div class="sensor-id">'+
+                            '<span>ID:'+val['sensor_id']+'</span>'+
+                        '</div>'+
+                        "<div class='sensor-num'>"+
+                            '<span>目前共有'+val['data'].length+'個感測器欄位</span>'+
+                        '</div>'+
+                        "<div class='sensor-edit'>"+
+                            '<a href="sensor/edit.html"><button type="button" class="btn btn-success sensor-edit-btn">編輯</button></a>'+
+                        '</div>'+
+                    '</div>'+
+                '</div>'
+                );
+
+        });
+
+
+        close();
+
+    });
+
+
+
+    close();
 })
