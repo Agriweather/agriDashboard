9920d5ebd2b0ae6e6891a5389eecc8af7138cb8d
SuWenYu <suwenyu@SuWenYuh.local>
2017-06-25 13:40:14 +0800

    add jquery api

--- Dashboard/js/index.js
+++ Dashboard/js/index.js
@@ -30,51 +30,63 @@ function close(){
     });
 }
 
+var url = 'http://demo.smartdog.com.tw/agri/';
 
 
-$('document').ready(function() {
 
-    $.getJSON( "https://suwenyu.github.io/test_sensor/sensor_data.json", function( data ) {
-        $.each( data, function( key, val ) {
-            console.log(key, val);
+$('document').ready(function(){
 
-            $('#sensor_data').append(
-                '<div class="col col-12 col-sm-6 col-md-4">'+
-                    '<div class="sensor-item">'+
-                        '<div class=row>'+
-                            '<div class="col-10">'+
-                                '<div class="sensor-mark">'+
-                                    'Sensor ' + val['sensor_num']+
-                                '</div>'+
-                            '</div>'+
-                            '<div class="col-2">'+
-                                '<div class="sensor-close F-right">'+
-                                    '<i class="fa fa-times"></i>'+
-                                '</div>'+
-                            '</div>'+
-                        '</div>'+
-                    '<hr class="sensor-hr">'+
-                        '<div class="sensor-id">'+
-                            '<span>ID:'+val['sensor_id']+'</span>'+
-                        '</div>'+
-                        "<div class='sensor-num'>"+
-                            '<span>目前共有'+val['data'].length+'個感測器欄位</span>'+
-                        '</div>'+
-                        "<div class='sensor-edit'>"+
-                            '<a href="sensor/edit.html"><button type="button" class="btn btn-success sensor-edit-btn">編輯</button></a>'+
-                        '</div>'+
-                    '</div>'+
-                '</div>'
-                );
-
-        });
-
-
-        close();
-
+    $.getJSON(url+'mobile/sensor/' ,function(raw_data){
+        console.log(raw_data);
     });
 
+}); 
 
 
-    close();
-})
+
+// $('document').ready(function() {
+
+//     $.getJSON( "https://suwenyu.github.io/test_sensor/sensor_data.json", function( data ) {
+//         $.each( data, function( key, val ) {
+//             console.log(key, val);
+
+//             $('#sensor_data').append(
+//                 '<div class="col col-12 col-sm-6 col-md-4">'+
+//                     '<div class="sensor-item">'+
+//                         '<div class=row>'+
+//                             '<div class="col-10">'+
+//                                 '<div class="sensor-mark">'+
+//                                     'Sensor ' + val['sensor_num']+
+//                                 '</div>'+
+//                             '</div>'+
+//                             '<div class="col-2">'+
+//                                 '<div class="sensor-close F-right">'+
+//                                     '<i class="fa fa-times"></i>'+
+//                                 '</div>'+
+//                             '</div>'+
+//                         '</div>'+
+//                     '<hr class="sensor-hr">'+
+//                         '<div class="sensor-id">'+
+//                             '<span>ID:'+val['sensor_id']+'</span>'+
+//                         '</div>'+
+//                         "<div class='sensor-num'>"+
+//                             '<span>目前共有'+val['data'].length+'個感測器欄位</span>'+
+//                         '</div>'+
+//                         "<div class='sensor-edit'>"+
+//                             '<a href="sensor/edit.html"><button type="button" class="btn btn-success sensor-edit-btn">編輯</button></a>'+
+//                         '</div>'+
+//                     '</div>'+
+//                 '</div>'
+//                 );
+
+//         });
+
+
+//         close();
+
+//     });
+
+
+
+//     close();
+// });
